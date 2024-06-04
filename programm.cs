07f1ee23ef832ae585b644398a151f16fbaaa197
M3rg <123715188+Mergezy@users.noreply.github.com>
2024-06-04 12:24:53 +0300

    Update programm.cs

--- programm.cs
+++ programm.cs
@@ -4,17 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        string name = "Игорь";
-        int age = 19;
-        int programCount = 48;
-        int money = 1000;
-        int time = 30;
-        bool truth = true;
-
-        Console.WriteLine("Здравствуйте, меня зовут " + name + ".");
-        Console.WriteLine("Мне " + age + " лет.");
-        Console.WriteLine("Я хочу научиться делать игры, это моя " + programCount + " программа на языке C#.");
-        Console.WriteLine("Когда я закончу обучение я хочу зарабатывать " + money + " денег в " + time + " наносекунд.");
-        Console.WriteLine("Достигну ли я желаемого? " + truth + "!");
+        sbyte a = -99;
+         ushort b = 55555;
+         uint c = 2394672295;
+         float d = 65.46f;
+         char e = 'a';
+         string f = "World";
     }
 }
